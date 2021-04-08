effc29557ac766f58f308008e2761d9bd4184e0e
MathieuMathurin <mathurin05@gmail.com>
2021-04-08 02:44:13 -0400

    feat: display the contact lists only if user is authenticated using github

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,63 @@
-export default function Home() {
+import { useState } from 'react';
+import { Paper, Container, makeStyles, Theme, createStyles, Typography } from '@material-ui/core';
+import withAuthentication from '../lib/withAuthentication';
+import { useContacts } from '../hooks/useContacts';
+import CreateContact from '../components/contacts/create-contact';
+import ContactsTable from '../components/contacts/contacts-table';
+import ManageContact from '../components/contacts/manage-contact';
+import Loader from '../components/loader';
+import { Contact } from '../models/contact';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    createContact: {
+      margin: '12px 0',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }
+  }),
+);
+
+function Home() {
+  const { contacts, isLoading, getContacts } = useContacts();
+  const classes = useStyles();
+  const [showUpdateComponent, setShowUpdateComponent] = useState(false);
+  const [selectedContact, setSelectedContact] = useState<Contact>();
+
+  const handleContactSelected = (contact: Contact) => {
+    setSelectedContact(contact);
+    setShowUpdateComponent(true);
+  };
+
+  const reset = () => {
+    setSelectedContact(undefined);
+    setShowUpdateComponent(false);
+  }
+
   return (
-    <div>
-      <main>
-        <p>This is a the home page</p>
-      </main>
-      </div>
-  )
+    <>
+      <Loader isLoading={isLoading} />
+      <Container maxWidth="lg">
+        <div className={classes.createContact}>
+        <Typography variant="h2">
+          Contacts
+        </Typography>
+          <CreateContact onContactCreated={getContacts} />
+          <ManageContact 
+            isDisplayed={showUpdateComponent} 
+            contact={selectedContact} 
+            onContactUpdated={() => {reset(); getContacts();}}
+            onContactDeleted={() => {reset(); getContacts();}}
+            onCancel={reset} 
+          />
+        </div>
+        <Paper>
+          <ContactsTable contacts={contacts} onContactClicked={handleContactSelected} />
+        </Paper>
+      </Container>
+    </>
+  );
 }
+
+export default withAuthentication(Home);
